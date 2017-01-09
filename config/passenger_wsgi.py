81332740d3b528647662ea87767162a4e987757b
birkin <birkin.diana@gmail.com>
2017-01-09 13:28:04 -0500

    updates env references.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -17,7 +17,7 @@ stuff
     iip_processing_project
         iip_processing_app
         config
-    env_min_djng
+    env_iip_prc
 """
 
 import os, pprint, sys
@@ -27,11 +27,11 @@ import os, pprint, sys
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 ## vars
-ACTIVATE_FILE = os.path.abspath( u'%s/../../env_min_djng/bin/activate_this.py' % current_directory )
+ACTIVATE_FILE = os.path.abspath( u'%s/../../env_iip_prc/bin/activate_this.py' % current_directory )
 PROJECT_DIR = os.path.abspath( u'%s/../../iip_processing_project' % current_directory )
 PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
 SETTINGS_MODULE = u'config.settings'
-SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_min_djng/lib/python2.7/site-packages' % current_directory )
+SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_iip_prc/lib/python2.7/site-packages' % current_directory )
 
 ## virtualenv
 execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
