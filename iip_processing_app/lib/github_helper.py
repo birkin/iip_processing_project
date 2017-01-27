9e9edf76b5f125387fdd42eeb48897994ef723c9
birkin <birkin.diana@gmail.com>
2017-01-27 13:27:29 -0500

    removes commented-out code.

--- iip_processing_app/lib/github_helper.py
+++ iip_processing_app/lib/github_helper.py
@@ -91,7 +91,6 @@ class GHHelper( object ):
         dir_segment = 'epidoc-files/'
         for entry in initial_list:
             if dir_segment in entry:  ## good inscription
-                # cleaned.append( entry.replace(dir_segment, '') )
                 cleaned.append( entry.replace(dir_segment, '').replace('.xml', '') )
         log.debug( 'cleaned, ```{}```'.format(cleaned) )
         return cleaned
