0651f1392baf949a490ff31868b6c6cf1a61a389
birkin <birkin.diana@gmail.com>
2017-01-31 13:51:00 -0500

    updates unit tests.

--- iip_processing_app/tests/tests_unit.py
+++ iip_processing_app/tests/tests_unit.py
@@ -15,6 +15,24 @@ gh_helper = GHHelper()
 # prepper = Prepper()
 
 
+class RootUrlTest(TestCase):
+    """ Checks root urls. """
+
+    def test_root_url_no_slash(self):
+        """ Checks '/root_url'. """
+        response = self.client.get( '' )  # project root part of url is assumed
+        self.assertEqual( 302, response.status_code )  # permanent redirect
+        redirect_url = response._headers['location'][1]
+        self.assertEqual(  '/info/', redirect_url )
+
+    def test_root_url_slash(self):
+        """ Checks '/root_url/'. """
+        response = self.client.get( '/' )  # project root part of url is assumed
+        self.assertEqual( 302, response.status_code )  # permanent redirect
+        redirect_url = response._headers['location'][1]
+        self.assertEqual(  '/info/', redirect_url )
+
+
 class HBAuthParserTest(TestCase):
     """ Checks parsing of http-basic-auth incoming info. """
 
--- iip_processing_app/tests_old.py
+++ iip_processing_app/tests_old.py
@@ -16,22 +16,22 @@ puller = Puller()
 prepper = Prepper()
 
 
-class RootUrlTest(TestCase):
-    """ Checks root urls. """
+# class RootUrlTest(TestCase):
+#     """ Checks root urls. """
 
-    def test_root_url_no_slash(self):
-        """ Checks '/root_url'. """
-        response = self.client.get( '' )  # project root part of url is assumed
-        self.assertEqual( 302, response.status_code )  # permanent redirect
-        redirect_url = response._headers['location'][1]
-        self.assertEqual(  '/info/', redirect_url )
+#     def test_root_url_no_slash(self):
+#         """ Checks '/root_url'. """
+#         response = self.client.get( '' )  # project root part of url is assumed
+#         self.assertEqual( 302, response.status_code )  # permanent redirect
+#         redirect_url = response._headers['location'][1]
+#         self.assertEqual(  '/info/', redirect_url )
 
-    def test_root_url_slash(self):
-        """ Checks '/root_url/'. """
-        response = self.client.get( '/' )  # project root part of url is assumed
-        self.assertEqual( 302, response.status_code )  # permanent redirect
-        redirect_url = response._headers['location'][1]
-        self.assertEqual(  '/info/', redirect_url )
+#     def test_root_url_slash(self):
+#         """ Checks '/root_url/'. """
+#         response = self.client.get( '/' )  # project root part of url is assumed
+#         self.assertEqual( 302, response.status_code )  # permanent redirect
+#         redirect_url = response._headers['location'][1]
+#         self.assertEqual(  '/info/', redirect_url )
 
 
 # class HBAuthParserTest(TestCase):
