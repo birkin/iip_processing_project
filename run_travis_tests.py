83523da80f9329af7acdeac90db8dd0a1f7c090d
birkin <birkin.diana@gmail.com>
2017-02-01 09:21:48 -0500

    initializes rest of required envvars.

--- run_travis_tests.py
+++ run_travis_tests.py
@@ -10,7 +10,19 @@ from django.test.utils import get_runner
 
 if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings_travis'
-    os.environ['IIP_PRC__CLONED_INSCRIPTIONS_PATH'] = 'foo'
+    ## initialize dummy envvars used by class initialization
+    for not_used_for_testing_item in [
+        'IIP_PRC__CLONED_INSCRIPTIONS_PATH',
+        'IIP_PRC__DISPLAY_STATUSES_BACKUP_DIR',
+        'IIP_PRC__SOLR_DOC_STYLESHEET_PATH',
+        'IIP_PRC__SOLR_URL',
+        'IIP_PRC__STATUSES_GIST_PASSWORD',
+        'IIP_PRC__STATUSES_GIST_URL',
+        'IIP_PRC__STATUSES_GIST_USERNAME',
+        'IIP_PRC__TRANSFORMER_AUTH_KEY',
+        'IIP_PRC__TRANSFORMER_URL' ]:
+        os.environ[ not_used_for_testing_item ] = 'foo'
+    os.environ['IIP_PRC__DISPLAY_STATUSES_BACKUP_TIMEFRAME_IN_DAYS'] = '1'  # converted to int by __init__
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
