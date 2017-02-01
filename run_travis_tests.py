962e262a3ff6a1bd095d795741e45e2954147102
birkin <birkin.diana@gmail.com>
2017-02-01 09:37:03 -0500

    added a few more not-used-but-required-for-testing envvars.

--- run_travis_tests.py
+++ run_travis_tests.py
@@ -12,15 +12,20 @@ if __name__ == '__main__':
     os.environ['DJANGO_SETTINGS_MODULE'] = 'config.settings_travis'
     ## initialize dummy envvars used by class initialization
     for not_used_for_testing_item in [
+        'IIP_PRC__BASIC_AUTH_PASSWORD',
+        'IIP_PRC__BASIC_AUTH_USERNAME',
         'IIP_PRC__CLONED_INSCRIPTIONS_PATH',
+        'IIP_PRC__DEV_URL',
         'IIP_PRC__DISPLAY_STATUSES_BACKUP_DIR',
+        'IIP_PRC__PRODUCTION_HOSTNAME',
         'IIP_PRC__SOLR_DOC_STYLESHEET_PATH',
         'IIP_PRC__SOLR_URL',
         'IIP_PRC__STATUSES_GIST_PASSWORD',
         'IIP_PRC__STATUSES_GIST_URL',
         'IIP_PRC__STATUSES_GIST_USERNAME',
         'IIP_PRC__TRANSFORMER_AUTH_KEY',
-        'IIP_PRC__TRANSFORMER_URL' ]:
+        'IIP_PRC__TRANSFORMER_URL',
+        ]:
         os.environ[ not_used_for_testing_item ] = 'foo'
     os.environ['IIP_PRC__DISPLAY_STATUSES_BACKUP_TIMEFRAME_IN_DAYS'] = '1'  # converted to int by __init__
     django.setup()
