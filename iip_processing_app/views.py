860781be30feb3525479fa15ea7b835678b27fce
birkin <birkin.diana@gmail.com>
2017-02-02 14:33:29 -0500

    calls refactored validator from view.

--- iip_processing_app/views.py
+++ iip_processing_app/views.py
@@ -8,10 +8,11 @@ from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.csrf import csrf_exempt
-from iip_processing_app.lib.github_helper import GHHelper
+from iip_processing_app.lib.github_helper import GHHelper, GHValidator
 
 
 log = logging.getLogger(__name__)
+github_validator = GHValidator()
 github_helper = GHHelper()
 
 
@@ -25,16 +26,15 @@ def info( request ):
 @csrf_exempt
 def gh_inscription_watcher( request ):
     """ Handles github inscriptions web-hook notification. """
-    log.debug( 'request.body, ```{}```'.format(pprint.pformat(request.body)) )
     log.debug( 'request.__dict__, ```{}```'.format(pprint.pformat(request.__dict__)) )
     resp = HttpResponseForbidden( '403 / Forbidden' )  # will be returned if incorrect basic-auth credentials are submitted
     if 'HTTP_AUTHORIZATION' in request.META:
-        received_username_password_dct = github_helper.parse_http_basic_auth( request.META['HTTP_AUTHORIZATION'].decode('utf-8') )
-        if github_helper.validate_credentials( received_username_password_dct ):
+        received_username_password_dct = github_validator.parse_http_basic_auth( request.META['HTTP_AUTHORIZATION'].decode('utf-8') )
+        if github_validator.validate_credentials( received_username_password_dct ):
             github_helper.handle_inscription_update( request.body, request.META.get('HTTP_HOST', None) )
             resp = HttpResponse( '200 / OK' )
     else:
-        resp = github_helper.make_unauthenticated_response()
+        resp = github_validator.make_unauthenticated_response()
     log.debug( 'response status code, `{}`'.format(resp.status_code) )
     return resp
 
