674f3e354ad21020afe4a71193572d0561ad1a71
birkin <birkin.diana@gmail.com>
2017-02-13 13:04:43 -0500

    adds valid json to travis initializer.

--- run_travis_tests.py
+++ run_travis_tests.py
@@ -17,7 +17,6 @@ if __name__ == '__main__':
         'IIP_PRC__CLONED_INSCRIPTIONS_PATH',
         'IIP_PRC__DEV_URL',
         'IIP_PRC__DISPLAY_STATUSES_BACKUP_DIR',
-        'IIP_PRC__LEGIT_ADMINS_JSON',
         'IIP_PRC__PRODUCTION_HOSTNAME',
         'IIP_PRC__REPO_SECRET_KEY',
         'IIP_PRC__SOLR_DOC_STYLESHEET_PATH',
@@ -30,6 +29,7 @@ if __name__ == '__main__':
         ]:
         os.environ[ not_used_for_testing_item ] = 'foo'
     os.environ['IIP_PRC__DISPLAY_STATUSES_BACKUP_TIMEFRAME_IN_DAYS'] = '1'  # converted to int by __init__
+    os.environ['IIP_PRC__LEGIT_ADMINS_JSON'] = '"foo"'  # valid json required
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
