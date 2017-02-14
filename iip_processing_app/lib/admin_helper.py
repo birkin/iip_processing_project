616e26bfa4af76c0354a23f05ef4967f02587f4d
birkin <birkin.diana@gmail.com>
2017-02-14 11:06:55 -0500

    implements solr-id grab.

--- iip_processing_app/lib/admin_helper.py
+++ iip_processing_app/lib/admin_helper.py
@@ -3,6 +3,7 @@
 from __future__ import unicode_literals
 
 import datetime, logging, os, pprint
+import requests
 
 
 log = logging.getLogger(__name__)
@@ -14,6 +15,7 @@ class OrphanDeleter( object ):
     def __init__( self ):
         """ Settings. """
         self.GIT_CLONED_DIR_PATH = unicode( os.environ['IIP_PRC__CLONED_INSCRIPTIONS_PATH'] )
+        self.SOLR_URL = unicode( os.environ['IIP_PRC__SOLR_URL'] )
 
     def prep_data( self ):
         """ Prepares list of ids to be deleted from solr.
@@ -23,6 +25,7 @@ class OrphanDeleter( object ):
         ## get file listing
         file_system_ids = self.build_directory_inscription_ids()
         ## get solr listing
+        solr_inscription_ids = self.build_solr_inscription_ids()
         ## find solr entries not in file list
         log.debug( 'data, ```{}```'.format(pprint.pformat(data)) )
         return data
@@ -40,6 +43,21 @@ class OrphanDeleter( object ):
         directory_inscription_ids = sorted( directory_inscription_ids )
         log.info( 'len(directory_inscription_ids), `{len}`; directory_inscription_ids[0:3], `{three}`'.format(len=len(directory_inscription_ids), three=pprint.pformat(directory_inscription_ids[0:3])) )
         return directory_inscription_ids
+
+    def build_solr_inscription_ids( self ):
+        """ Returns list of solr inscription ids.
+            Called by prep_data(). """
+        url = '{}/select?q=*:*&fl=inscription_id&rows=100000&wt=json'.format( self.SOLR_URL )
+        log.info( 'url, ```{}```'.format(url) )
+        r = requests.get( url )
+        json_dict = r.json()
+        docs = json_dict['response']['docs']  # list of dicts
+        solr_inscription_ids = []
+        for doc in docs:
+            solr_inscription_ids.append( doc['inscription_id'] )
+        solr_inscription_ids = sorted( solr_inscription_ids )
+        log.info( 'len(solr_inscription_ids), `{len}`; solr_inscription_ids[0:3], `{three}`'.format(len=len(solr_inscription_ids), three=pprint.pformat(solr_inscription_ids[0:3])) )
+        return solr_inscription_ids
 
     def prep_context( self, data ):
         """ Prepares response info.
