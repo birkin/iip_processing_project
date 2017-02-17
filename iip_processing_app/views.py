8c63de0a5fe68af1415acc05b711b18ef9d7641c
birkin <birkin.diana@gmail.com>
2017-02-17 12:05:03 -0500

    updates host-check.

--- iip_processing_app/views.py
+++ iip_processing_app/views.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 import datetime, json, logging, os, pprint
-from django.conf import settings as project_settings
+from django.conf import settings
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseForbidden, HttpResponseRedirect
@@ -80,7 +80,7 @@ def update_processing_status( request ):
     """ Updates status table. """
     log.debug( 'request.__dict__, ```{}```'.format(pprint.pformat(request.__dict__)) )
     resp = HttpResponseForbidden( '403 / Forbidden' )
-    if unicode( request.META.get('HTTP_HOST', '') ) == '127.0.0.1':
+    if unicode( request.META.get('HTTP_HOST', '') ) in settings.ALLOWED_HOSTS:
         ( to_process_dct, single_update_dct ) = process_status_recorder.check_for_data( request.body )
         if to_process_dct:
             resp = process_status_recorder.handle_enqueues( to_process_dct )
