ea59482a1e4cd56b08a8060e1ec5bafc245375de
birkin <birkin.diana@gmail.com>
2017-02-22 12:58:56 -0500

    initializes dummy env-vars for travis.

--- run_travis_tests.py
+++ run_travis_tests.py
@@ -17,6 +17,8 @@ if __name__ == '__main__':
         'IIP_PRC__CLONED_INSCRIPTIONS_PATH',
         'IIP_PRC__DEV_URL',
         'IIP_PRC__DISPLAY_STATUSES_BACKUP_DIR',
+        'IIP_PRC__LEGIT_QUEUE_VIEWER_PASSWORD',
+        'IIP_PRC__LEGIT_QUEUE_VIEWER_USER',
         'IIP_PRC__PROCESS_STATUS_UPDATER_URL',
         'IIP_PRC__PRODUCTION_HOSTNAME',
         'IIP_PRC__REPO_SECRET_KEY',
@@ -31,6 +33,8 @@ if __name__ == '__main__':
         os.environ[ not_used_for_testing_item ] = 'foo'
     os.environ['IIP_PRC__DISPLAY_STATUSES_BACKUP_TIMEFRAME_IN_DAYS'] = '1'  # converted to int by __init__
     os.environ['IIP_PRC__LEGIT_ADMINS_JSON'] = '"foo"'  # valid json required
+    os.environ['IIP_PRC__LEGIT_QUEUE_VIEWER_GROUPS_JSON'] = '"foo"'
+    os.environ['IIP_PRC__LEGIT_QUEUE_VIEWER_EPPNS_JSON'] = '"foo"'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
