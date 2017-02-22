53ffdd75f2fe3809aa60ea767b9d89022dbf72d0
birkin <birkin.diana@gmail.com>
2017-02-22 08:26:42 -0500

    lengthens inscription-id db-field.

--- config/passenger_wsgi.py
+++ config/passenger_wsgi.py
@@ -22,40 +22,34 @@ stuff
 
 import os, pprint, sys
 
-try:
+## become self-aware, padawan
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
-    ## become self-aware, padawan
-    current_directory = os.path.dirname(os.path.abspath(__file__))
+## vars
+ACTIVATE_FILE = os.path.abspath( u'%s/../../env_iip_prc/bin/activate_this.py' % current_directory )
+PROJECT_DIR = os.path.abspath( u'%s/../../iip_processing_project' % current_directory )
+PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
+SETTINGS_MODULE = u'config.settings'
+SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_iip_prc/lib/python2.7/site-packages' % current_directory )
 
-    ## vars
-    ACTIVATE_FILE = os.path.abspath( u'%s/../../env_iip_prc/bin/activate_this.py' % current_directory )
-    PROJECT_DIR = os.path.abspath( u'%s/../../iip_processing_project' % current_directory )
-    PROJECT_ENCLOSING_DIR = os.path.abspath( u'%s/../..' % current_directory )
-    SETTINGS_MODULE = u'config.settings'
-    SITE_PACKAGES_DIR = os.path.abspath( u'%s/../../env_iip_prc/lib/python2.7/site-packages' % current_directory )
+## virtualenv
+execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
 
-    ## virtualenv
-    execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )
+## sys.path additions
+for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
+ if entry not in sys.path:
+   sys.path.append( entry )
 
-    ## sys.path additions
-    for entry in [PROJECT_DIR, PROJECT_ENCLOSING_DIR, SITE_PACKAGES_DIR]:
-     if entry not in sys.path:
-       sys.path.append( entry )
+## environment additions
+os.environ[u'DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
 
-    ## environment additions
-    os.environ[u'DJANGO_SETTINGS_MODULE'] = SETTINGS_MODULE  # so django can access its settings
+## load up env vars
+SETTINGS_FILE = os.environ['IIP_PRC__SETTINGS_PATH']  # set in activate_this.py, and activated above
+import shellvars
+var_dct = shellvars.get_vars( SETTINGS_FILE )
+for ( key, val ) in var_dct.items():
+    os.environ[key] = val
 
-    ## load up env vars
-    SETTINGS_FILE = os.environ['IIP_PRC__SETTINGS_PATH']  # set in activate_this.py, and activated above
-    import shellvars
-    var_dct = shellvars.get_vars( SETTINGS_FILE )
-    for ( key, val ) in var_dct.items():
-        os.environ[key] = val
-
-    ## gogogo
-    from django.core.wsgi import get_wsgi_application
-    application = get_wsgi_application()
-
-except Exception as e:
-
-    print 'error, ```{}```'.format( repr(e) )
+## gogogo
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
