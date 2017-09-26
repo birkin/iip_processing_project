5c8bebc60a8f71307038f17f9f9bec2c078de3c2
birkin <birkin.diana@gmail.com>
2017-09-26 13:32:21 -0400

    refactors call to validator.

--- iip_processing_app/lib/process_all_helper.py
+++ iip_processing_app/lib/process_all_helper.py
@@ -3,11 +3,13 @@
 from __future__ import unicode_literals
 
 import json, logging, os
-from iip_pro
-1.0
+from iip_processing_app.lib.orphan_helper import OrphanDeleter
+from iip_processing_app.lib.processor import Puller
 
 
 log = logging.getLogger(__name__)
+validator = OrphanDeleter()  # yeah, weird, see function's TODO
+puller = Puller()
 
 
 class AllProcessorHelper(object):
@@ -21,18 +23,14 @@ class AllProcessorHelper(object):
         """ Validates admin request.
             Called by views.delete_solr_orphans()
             TODO: refactor to common helper with orphan_helper.OrphanDeleter.validate_delete_request() """
-        validity = False
-        if eppn in self.ADMINS:
-            validity = True
-        elif dev_user in self.ADMINS and host == '127.0.0.1':
-            validity = True
+        validity = validator.validate_delete_request( eppn, dev_user, host )  # yeah, it's not a delete; see docstring TODO
         log.debug( 'validity, `%s`' % validity )
         return validity
 
     def prep_data( self ):
         """ Prepares list of ids to be indexed.
             Called by views.process_all() """
-        self.call_git_pull()
+        puller.call_git_pull()
         file_system_ids = self.build_directory_inscription_ids()
         log.debug( 'len(file_system_ids), `%`' % len(file_system_ids) )
         return file_system_ids
