3ea8e9db4fda49b77d9dae6e5e0bbae658c413aa
birkin <birkin.diana@gmail.com>
2017-09-26 11:12:17 -0400

    adds request validation.

--- iip_processing_app/lib/process_all_helper.py
+++ iip_processing_app/lib/process_all_helper.py
@@ -2,7 +2,7 @@
 
 from __future__ import unicode_literals
 
-import logging
+import json, logging, os
 
 
 log = logging.getLogger(__name__)
@@ -13,4 +13,16 @@ class AllProcessorHelper(object):
         Helper for views.process_all() """
 
     def __init__( self ):
-        pass
+        self.ADMINS = json.loads( os.environ['IIP_PRC__LEGIT_ADMINS_JSON'] )
+
+    def validate_request( self, eppn, dev_user, host ):
+        """ Validates admin request.
+            Called by views.delete_solr_orphans()
+            TODO: refactor to common helper with orphan_helper.OrphanDeleter.validate_delete_request() """
+        validity = False
+        if eppn in self.ADMINS:
+            validity = True
+        elif dev_user in self.ADMINS and host == '127.0.0.1':
+            validity = True
+        log.debug( 'validity, `%s`' % validity )
+        return validity
