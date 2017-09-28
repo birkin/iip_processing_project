21e4cc15e442f0ffa32df8f64e04409355784895
birkin <birkin.diana@gmail.com>
2017-09-28 11:52:37 -0400

    flake8 improvements.

--- iip_processing_app/lib/queue_utils/clean_failed_queue.py
+++ iip_processing_app/lib/queue_utils/clean_failed_queue.py
@@ -15,34 +15,34 @@ import redis, rq
 QUEUE_NAME = unicode( os.environ['IIP_PRC__QUEUE_NAME'] )
 
 
-failed_queue = rq.queue.get_failed_queue( connection=redis.Redis(u'localhost') )
+failed_queue = rq.queue.get_failed_queue( connection=redis.Redis('localhost') )
 
-d = { u'jobs': [] }
+d = { 'jobs': [] }
 failed_count = 0
 for job in failed_queue.jobs:
     if not job.origin == QUEUE_NAME:
         continue
     failed_count += 1
     job_d = {
-        u'_args': job._args,
-        u'_kwargs': job._kwargs,
-        u'_func_name': job._func_name,
-        u'description': job.description,
-        u'dt_created': job.created_at,
-        u'dt_enqueued': job.enqueued_at,
-        u'dt_ended': job.ended_at,
-        u'origin': job.origin,
-        u'id': job._id,
-        u'traceback': job.exc_info,
-        u'meta': job.meta,
-        u'_result': job._result,
-        u'_status': job._status,
-        }
-    d[u'jobs'].append( job_d )
+        '_args': job._args,
+        '_kwargs': job._kwargs,
+        '_func_name': job._func_name,
+        'description': job.description,
+        'dt_created': job.created_at,
+        'dt_enqueued': job.enqueued_at,
+        'dt_ended': job.ended_at,
+        'origin': job.origin,
+        'id': job._id,
+        'traceback': job.exc_info,
+        'meta': job.meta,
+        '_result': job._result,
+        '_status': job._status,
+    }
+    d['jobs'].append( job_d )
     job.delete()
-d[u'initial_failed_target_count'] = failed_count
+d['initial_failed_target_count'] = failed_count
 
 q2 = rq.Queue( QUEUE_NAME, connection=redis.Redis() )
-d[u'current_failed_target_count'] = len(q2.jobs)
+d['current_failed_target_count'] = len(q2.jobs)
 
 pprint.pprint( d )
--- iip_processing_app/lib/queue_utils/clear_job_queue.py
+++ iip_processing_app/lib/queue_utils/clear_job_queue.py
@@ -11,24 +11,24 @@ QUEUE_NAME = unicode( os.environ['IIP_PRC__QUEUE_NAME'] )
 
 q = rq.Queue( QUEUE_NAME, connection=redis.Redis() )
 
-print u'- initial number of jobs in queue `%s`: %s' % ( QUEUE_NAME, len(q.jobs) )
+print '- initial number of jobs in queue `%s`: %s' % ( QUEUE_NAME, len(q.jobs) )
 
 for job in q.jobs:
     job_d = {
-        u'args': job._args,
-        u'kwargs': job._kwargs,
-        u'function': job._func_name,
-        u'description': job.description,
-        u'dt_created': job.created_at,
-        u'dt_enqueued': job.enqueued_at,
-        u'dt_ended': job.ended_at,
-        u'origin': job.origin,
-        u'id': job._id,
-        u'traceback': job.exc_info
-        }
-    print u'- job info...'; pprint.pprint( job_d )
+        'args': job._args,
+        'kwargs': job._kwargs,
+        'function': job._func_name,
+        'description': job.description,
+        'dt_created': job.created_at,
+        'dt_enqueued': job.enqueued_at,
+        'dt_ended': job.ended_at,
+        'origin': job.origin,
+        'id': job._id,
+        'traceback': job.exc_info
+    }
+    print '- job info...'; pprint.pprint( job_d )
     job.delete()
-    print u'- deleted.'
-    print u'---'
+    print '- deleted.'
+    print '---'
 
-print u'- current number of jobs in queue `%s`: %s' % ( QUEUE_NAME, len(q.jobs) )
+print '- current number of jobs in queue `%s`: %s' % ( QUEUE_NAME, len(q.jobs) )
