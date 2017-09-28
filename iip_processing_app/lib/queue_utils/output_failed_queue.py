3cd43c6cb064c9404f099898868ae71671ea48f0
birkin <birkin.diana@gmail.com>
2017-09-28 11:54:27 -0400

    flake8 improvements.

--- iip_processing_app/lib/queue_utils/output_failed_queue.py
+++ iip_processing_app/lib/queue_utils/output_failed_queue.py
@@ -9,29 +9,29 @@ import redis, rq
 QUEUE_NAME = unicode( os.environ['IIP_PRC__QUEUE_NAME'] )
 
 
-failed_queue = rq.queue.get_failed_queue( connection=redis.Redis(u'localhost') )
+failed_queue = rq.queue.get_failed_queue( connection=redis.Redis('localhost') )
 
-d = { u'failed_target_count': None, u'jobs': [] }
+d = { 'failed_target_count': None, 'jobs': [] }
 failed_count = 0
 for job in failed_queue.jobs:
     if not job.origin == QUEUE_NAME:
         continue
     failed_count += 1
     job_d = {
-        u'_args': job._args,
-        u'_kwargs': job._kwargs,
-        u'_func_name': job._func_name,
-        u'description': job.description,
-        u'dt_created': job.created_at,
-        u'dt_enqueued': job.enqueued_at,
-        u'dt_ended': job.ended_at,
-        u'origin': job.origin,
-        u'id': job._id,
-        u'traceback': job.exc_info,
-        u'meta': job.meta,
-        u'_result': job._result,
-        u'_status': job._status,
-        }
-    d[u'jobs'].append( job_d )
-d[u'failed_target_count'] = failed_count
+        '_args': job._args,
+        '_kwargs': job._kwargs,
+        '_func_name': job._func_name,
+        'description': job.description,
+        'dt_created': job.created_at,
+        'dt_enqueued': job.enqueued_at,
+        'dt_ended': job.ended_at,
+        'origin': job.origin,
+        'id': job._id,
+        'traceback': job.exc_info,
+        'meta': job.meta,
+        '_result': job._result,
+        '_status': job._status,
+    }
+    d['jobs'].append( job_d )
+d['failed_target_count'] = failed_count
 pprint.pprint( d )
--- iip_processing_app/lib/queue_utils/output_job_queue.py
+++ iip_processing_app/lib/queue_utils/output_job_queue.py
@@ -11,23 +11,23 @@ QUEUE_NAME = unicode( os.environ['IIP_PRC__QUEUE_NAME'] )
 
 q = rq.Queue( QUEUE_NAME, connection=redis.Redis() )
 
-print u'- number of jobs in queue `%s`: %s' % ( QUEUE_NAME, len(q.jobs) )
+print '- number of jobs in queue `%s`: %s' % ( QUEUE_NAME, len(q.jobs) )
 
 for job in q.jobs:
     job_d = {
-        u'_args': job._args,
-        u'_kwargs': job._kwargs,
-        u'_func_name': job._func_name,
-        u'description': job.description,
-        u'dt_created': job.created_at,
-        u'dt_enqueued': job.enqueued_at,
-        u'dt_ended': job.ended_at,
-        u'origin': job.origin,
-        u'id': job._id,
-        u'traceback': job.exc_info,
-        u'meta': job.meta,
-        u'_result': job._result,
-        u'_status': job._status,
-        }
-    print u'- job info...'; pprint.pprint( job_d )
-    print u'---'
+        '_args': job._args,
+        '_kwargs': job._kwargs,
+        '_func_name': job._func_name,
+        'description': job.description,
+        'dt_created': job.created_at,
+        'dt_enqueued': job.enqueued_at,
+        'dt_ended': job.ended_at,
+        'origin': job.origin,
+        'id': job._id,
+        'traceback': job.exc_info,
+        'meta': job.meta,
+        '_result': job._result,
+        '_status': job._status,
+    }
+    print '- job info...'; pprint.pprint( job_d )
+    print '---'
